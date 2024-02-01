c6141b46ac147e05f559878155f7e81ca7921ef1
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2024-02-01 15:20:12 +0600

    fifth

--- easyfsl/modules/feat_resnet12.py
+++ easyfsl/modules/feat_resnet12.py
@@ -10,123 +10,6 @@ from torchvision.models.resnet import conv3x3
 
 import math
 
-from tensorflow.keras import layers
-import tensorflow as tf
-from keras import backend
-
-
-def squeeze_excitation_block(input_layer, out_dim, ratio=32, name=None):
-	"""Squeeze and Extraction block
-   Args:
-      input_layer: input tensor
-      out_dim: integer, output dimension for the model
-      ratio: integer, reduction ratio for the number of neurons in the hidden layers
-      name: string, block label
-    Returns:
-      Output A tensor for the squeeze and excitation block
-    """
-
-	#  Get the number of channels of the input characteristic graph
-	in_channel = input_layer.shape[-1]
-
-	#  Global average pooling [h,w,c]==>[None,c]
-	squeeze = layers.GlobalAveragePooling2D(name=name + "_Squeeze_GlobalPooling")(input_layer)
-	# [None,c]==>[1,1,c]
-	squeeze = layers.Reshape(target_shape=(1, 1, in_channel))(squeeze)
-
-	excitation = layers.Dense(units=out_dim / ratio, name=name + "_Excitation_FC_1")(squeeze)
-	excitation = layers.Activation('relu', name=name + '_Excitation_FC_Relu_1')(excitation)
-
-	excitation = layers.Dense(out_dim, name=name + "_Excitation_FC_2")(excitation)
-	excitation = layers.Activation('sigmoid', name=name + '_Excitation_FC_Relu_2')(excitation)
-	excitation = tf.reshape(excitation, [-1, 1, 1, out_dim])
-
-	scale = layers.multiply([input_layer, excitation])
-
-	return scale
-
-
-def ECA_Net_block(input_layer, kernel_size=3, adaptive=False, name=None):
-	"""ECA-Net: Efficient Channel Attention Block
-    Args:
-      input_layer: input tensor
-      kernel_size: integer, default: 3, size of the kernel for the convolution
-      adaptive: bool, default false , set kernel size depending on the number of input channels
-      name: string, block label
-    Returns:
-      Output A tensor for the ECA-Net attention block
-    """
-	if adaptive:
-		b = 1
-		gamma = 2
-		channels = input_layer.shape[-1]
-		kernel_size = int(abs((math.log2(channels) + b / gamma)))
-		if (kernel_size % 2) == 0:
-			kernel_size = kernel_size + 1
-		else:
-			kernel_size = kernel_size
-
-	squeeze = layers.GlobalAveragePooling2D(name=name + "_Squeeze_GlobalPooling")(input_layer)
-	squeeze = tf.expand_dims(squeeze, axis=1)
-	excitation = layers.Conv1D(filters=1,
-							   kernel_size=kernel_size,
-							   padding='same',
-							   use_bias=False,
-							   name=name + "_Excitation_Conv_1D")(squeeze)
-
-	excitation = tf.expand_dims(tf.transpose(excitation, [0, 2, 1]), 3)
-	excitation = tf.math.sigmoid(excitation)
-
-	output = layers.multiply([input_layer, excitation])
-
-	return output
-
-
-def CBAM_block(input_layer, filter_num, reduction_ratio=32, kernel_size=7, name=None):
-	"""CBAM: Convolutional Block Attention Module Block
-    Args:
-      input_layer: input tensor
-      filter_num: integer, number of neurons in the hidden layers
-      reduction_ratio: integer, default 32,reduction ratio for the number of neurons in the hidden layers
-      kernel_size: integer, default 7, kernel size of the spatial convolution excitation convolution
-      name: string, block label
-    Returns:
-      Output A tensor for the CBAM attention block
-    """
-	axis = -1
-
-	# CHANNEL ATTENTION
-	avg_pool = layers.GlobalAveragePooling2D(name=name + "_Chanel_AveragePooling")(input_layer)
-	max_pool = layers.GlobalMaxPool2D(name=name + "_Chanel_MaxPooling")(input_layer)
-
-	# Shared MLP
-	dense1 = layers.Dense(filter_num // reduction_ratio, activation='relu', name=name + "_Chanel_FC_1")
-	dense2 = layers.Dense(filter_num, name=name + "_Chanel_FC_2")
-
-	avg_out = dense2(dense1(avg_pool))
-	max_out = dense2(dense1(max_pool))
-
-	channel = layers.add([avg_out, max_out])
-	channel = layers.Activation('sigmoid', name=name + "_Chanel_Sigmoid")(channel)
-	channel = layers.Reshape((1, 1, filter_num), name=name + "_Chanel_Reshape")(channel)
-
-	channel_output = layers.multiply([input_layer, channel])
-
-	# SPATIAL ATTENTION
-	avg_pool2 = tf.reduce_mean(input_layer, axis=axis, keepdims=True)
-	max_pool2 = tf.reduce_max(input_layer, axis=axis, keepdims=True)
-
-	spatial = layers.concatenate([avg_pool2, max_pool2], axis=axis)
-
-	# K = 7 achieves the highest accuracy
-	spatial = layers.Conv2D(1, kernel_size=kernel_size, padding='same', name=name + "_Spatial_Conv2D")(spatial)
-	spatial_out = layers.Activation('sigmoid', name=name + "_Spatial_Sigmoid")(spatial)
-
-	CBAM_out = layers.multiply([channel_output, spatial_out])
-
-	return CBAM_out
-
-
 class FEATBasicBlock(nn.Module):
     """
     BasicBlock for FEAT. Uses 3 convolutions instead of 2, a LeakyReLU instead of ReLU, and a MaxPool2d.
@@ -210,7 +93,7 @@ class FEATResNet12(nn.Module):
         self.inplanes = 3
         super().__init__()
 
-        channels = [64, 160, 320]
+        channels = [64, 160, 320, 320, 640]
         self.layer_dims = [
             channels[i] * block.expansion for i in range(3) for j in range(3)
         ]
@@ -231,19 +114,16 @@ class FEATResNet12(nn.Module):
             stride=2,
         )
 
-        self.localization = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=7),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True),
-            nn.Conv2d(8, 10, kernel_size=5),
-            nn.MaxPool2d(2, stride=2),
-            nn.ReLU(True)
+        self.localization = self._make_localization_layer(
+            block,
+            320,
+            stride=2,
         )
 
-        self.fc_loc = nn.Sequential(
-            nn.Linear(640, 32),
-            nn.ReLU(True),
-            nn.Linear(32, 3 * 2)
+        self.fc_loc = self._make_fc_layer(
+            block,
+            640,
+            stride=2,
         )
         
 
@@ -266,6 +146,66 @@ class FEATResNet12(nn.Module):
         grid = nn.functional.affine_grid(theta, x.size())
         x = nn.functional.grid_sample(x, grid)
         return x
+
+    def _make_localization_layer(self, block, planes, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=7,
+                    stride=1,
+                    bias=False,
+                ),
+                nn.MaxPool2d(2, stride=2),
+                nn.ReLU(True),
+                nn.Conv2d(
+                    self.inplanes,
+                    planes * block.expansion,
+                    kernel_size=5,
+                    stride=1,
+                    bias=False,
+                ),
+                nn.MaxPool2d(2, stride=2),
+                nn.ReLU(True),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+            )
+        )
+        self.inplanes = planes * block.expansion
+
+        return nn.Sequential(*layers)
+    
+    def _make_fc_layer(self, block, planes, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Linear(640, 32),
+                nn.ReLU(True),
+                nn.Linear(32, 3 * 2),
+            )
+
+        layers = []
+        layers.append(
+            block(
+                self.inplanes,
+                planes,
+                stride,
+                downsample,
+            )
+        )
+        self.inplanes = planes * block.expansion
+
+        return nn.Sequential(*layers)
 
     def _make_layer(self, block, planes, stride=1):
         downsample = None
