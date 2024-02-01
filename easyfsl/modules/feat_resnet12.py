71307691473f078d9407473dd447c8161e0c51fe
Adnan Ferdous Ashrafi <nazib91@yahoo.com>
2024-02-01 15:40:01 +0600

    ninth

--- easyfsl/modules/feat_resnet12.py
+++ easyfsl/modules/feat_resnet12.py
@@ -221,7 +221,7 @@ class FEATResNet12(nn.Module):
         """
         Iterate over the blocks and apply them sequentially.
         """
-        x = self.layer3(self.layer2(self.layer1(self.localization(x))))
+        x = self.localization(self.layer3(self.layer2(self.layer1(x))))
         return x.mean((-2, -1))
 
 
